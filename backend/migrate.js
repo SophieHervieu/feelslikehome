53b0373fd38d3598912e2ebfd798a7725d027c50
SophieHervieu <sophie-hervieu@live.fr>
2025-06-23 23:25:17 +0200

    modification dans migrate.js pour tenter d'avoir les bonnes variables

--- backend/migrate.js
+++ backend/migrate.js
@@ -6,11 +6,13 @@ const mysql = require('mysql2/promise')
 const migrate = async () => {
   const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 
+  const password = DB_PASSWORD ?? '';
+
   const connection = await mysql.createConnection({
     host: DB_HOST,
     port: DB_PORT,
     user: DB_USER,
-    password: DB_PASSWORD,
+    password,
     multipleStatements: true,
   })
 
