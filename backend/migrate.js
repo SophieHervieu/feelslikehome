e7d608635406691319338cde383fd3afea87bd12
SophieHervieu <sophie-hervieu@live.fr>
2025-06-23 23:43:57 +0200

    essai numéro desespoir

--- backend/migrate.js
+++ backend/migrate.js
@@ -6,11 +6,13 @@ const mysql = require('mysql2/promise')
 const migrate = async () => {
   const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 
+  const password = typeof DB_PASSWORD === 'undefined' || DB_PASSWORD === '""' ? '' : DB_PASSWORD;
+
   const connection = await mysql.createConnection({
     host: DB_HOST,
     port: DB_PORT,
     user: DB_USER,
-    password: typeof DB_PASSWORD === 'undefined' ? '' : DB_PASSWORD,
+    password,
     multipleStatements: true,
   })
 
