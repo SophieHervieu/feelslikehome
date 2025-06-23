1249b73aafc98f3ef7983cbd6b02ddb5744e778c
SophieHervieu <sophie-hervieu@live.fr>
2025-06-23 23:30:42 +0200

    encore une tentative pour que ça fonctionne peut-etre

--- backend/migrate.js
+++ backend/migrate.js
@@ -6,13 +6,11 @@ const mysql = require('mysql2/promise')
 const migrate = async () => {
   const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 
-  const password = DB_PASSWORD ?? '';
-
   const connection = await mysql.createConnection({
     host: DB_HOST,
     port: DB_PORT,
     user: DB_USER,
-    password,
+    password: typeof DB_PASSWORD === 'undefined' ? '' : DB_PASSWORD,
     multipleStatements: true,
   })
 
